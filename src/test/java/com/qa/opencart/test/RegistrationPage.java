4ada48c4f43fa84b036f2bc2aeb958d74ff1e413
Thangaprathapkumar <thangaprathap.056@gmail.com>
2025-03-27 12:47:12 +0530

    registraion check

---
+++ src/test/java/com/qa/opencart/test/RegistrationPage.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.test;
+
+import org.testng.annotations.Test;
+
+import com.qa.opencart.base.BaseTest;
+
+public class RegistrationPage extends BaseTest{
+	
+	
+	@Test
+	public void getRegisterPage() {
+		
+	}
+
+}
